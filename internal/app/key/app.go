26bbd137d80cd2d19b271674fc194f3a2a7cde6a
Saveliy Tretyakov <Tretyakov.sava@gmail.com>
2025-01-23 20:24:55 +0300

    move lib to pkg

--- internal/app/key/app.go
+++ internal/app/key/app.go
@@ -2,10 +2,8 @@ package key
 
 import (
 	"crypto/ecdsa"
-	"crypto/x509"
-	"encoding/pem"
 	"log/slog"
-	"sso/internal/lib/jwt"
+	"sso/pkg/lib/jwt"
 	"time"
 )
 
@@ -57,25 +55,25 @@ func (k *App) GetPrivateKeyChan() <-chan *ecdsa.PrivateKey {
 	return k.privateKeyChan
 }
 
-func encode(privateKey *ecdsa.PrivateKey, publicKey *ecdsa.PublicKey) (string, string) {
-	x509Encoded, _ := x509.MarshalECPrivateKey(privateKey)
-	pemEncoded := pem.EncodeToMemory(&pem.Block{Type: "PRIVATE KEY", Bytes: x509Encoded})
+// func encode(privateKey *ecdsa.PrivateKey, publicKey *ecdsa.PublicKey) (string, string) {
+// 	x509Encoded, _ := x509.MarshalECPrivateKey(privateKey)
+// 	pemEncoded := pem.EncodeToMemory(&pem.Block{Type: "PRIVATE KEY", Bytes: x509Encoded})
 
-	x509EncodedPub, _ := x509.MarshalPKIXPublicKey(publicKey)
-	pemEncodedPub := pem.EncodeToMemory(&pem.Block{Type: "PUBLIC KEY", Bytes: x509EncodedPub})
+// 	x509EncodedPub, _ := x509.MarshalPKIXPublicKey(publicKey)
+// 	pemEncodedPub := pem.EncodeToMemory(&pem.Block{Type: "PUBLIC KEY", Bytes: x509EncodedPub})
 
-	return string(pemEncoded), string(pemEncodedPub)
-}
+// 	return string(pemEncoded), string(pemEncodedPub)
+// }
 
-func decode(pemEncoded string, pemEncodedPub string) (*ecdsa.PrivateKey, *ecdsa.PublicKey) {
-	block, _ := pem.Decode([]byte(pemEncoded))
-	x509Encoded := block.Bytes
-	privateKey, _ := x509.ParseECPrivateKey(x509Encoded)
+// func decode(pemEncoded string, pemEncodedPub string) (*ecdsa.PrivateKey, *ecdsa.PublicKey) {
+// 	block, _ := pem.Decode([]byte(pemEncoded))
+// 	x509Encoded := block.Bytes
+// 	privateKey, _ := x509.ParseECPrivateKey(x509Encoded)
 
-	blockPub, _ := pem.Decode([]byte(pemEncodedPub))
-	x509EncodedPub := blockPub.Bytes
-	genericPublicKey, _ := x509.ParsePKIXPublicKey(x509EncodedPub)
-	publicKey := genericPublicKey.(*ecdsa.PublicKey)
+// 	blockPub, _ := pem.Decode([]byte(pemEncodedPub))
+// 	x509EncodedPub := blockPub.Bytes
+// 	genericPublicKey, _ := x509.ParsePKIXPublicKey(x509EncodedPub)
+// 	publicKey := genericPublicKey.(*ecdsa.PublicKey)
 
-	return privateKey, publicKey
-}
+// 	return privateKey, publicKey
+// }
